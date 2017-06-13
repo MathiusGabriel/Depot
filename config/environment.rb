5a22bc0839ba8c576075f0cf100e8441251a9aa7
Maciej <maciej.kurdwanowski@gmail.com>
2017-06-13 09:07:59 +0200

    Commit Mac

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Depot::Application.initialize!
+
+Depot::Application.configure do
+	config.action_mailer.delivery_method = :test
+	#config.action_mailer.smtp_settings = {
+	#	adress: "smtp.mathius.atthouse.pl",
+	#	port: 587,
+	#	authentication: "plain",
+	#	user_name: "test@mkarcheo.com",
+	#	password: "Test.112",
+	#	enable_starttls_auto: true
+#
+#	}
+	
+end
