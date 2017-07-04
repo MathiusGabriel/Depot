2de1fed54ee085c60f2664d20ac0cfbd217a6811
Matt Kurdwanowski <maciej.kurdwanowski@gmail.com>
2017-07-04 17:14:03 +0200

    temp commint

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,12 @@
 class Product < ActiveRecord::Base
+	before_destroy :ensure_not_referenced_by_any_line_item
+
 	has_many :line_items
 	has_many :orders, through: :line_items
 
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
-	validates :title, uniqueness: true 
+	validates :title, uniqueness: true
 	validates :title, length: {
 		minimum: 3,
 		too_short: "must be at least 3 characters long"}
@@ -12,22 +14,25 @@ class Product < ActiveRecord::Base
 		with: %r{\.(gif|jpg|png)\Z}i,
 		message: 'Must be a URL for GIF, JPG or PNG image'
 	}
-	
+
+=begin
+Moje pytanie dotyczące dodatkowych testów tyczy się części modelu, która jest poniżej,
+bo to co napisałem w specu generalnie obejmuje to co powyżej. Czy to też w jakiś sposób powinno być przetestowane
+w specu z testami modelu?
+=end
+
 	def self.latest
 		Product.order(:updated_at).last
 	end
 
-	before_destroy :ensure_not_referenced_by_any_line_item
-
 	private
-
 	#ensure that there are no line items referencing to this product
-	def ensure_not_referenced_by_any_line_item
-		if line_items.empty?
-			return true
-		else
-			errors.add(:base, 'Line Items present')
-			return false
+		def ensure_not_referenced_by_any_line_item
+			if line_items.empty?
+				return true
+			else
+				errors.add(:base, 'Line Items present')
+				return false
+			end
 		end
-	end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,70 +1,64 @@
-require 'spec_helper'
+require 'rails_helper'
 
-class ProductTest < ActiveSupport::TestCase
-	fixtures :products
-  test "product attributes must not be empty" do
-  	product = Product.new
-  	assert product.invalid?
-  	assert product.errors[:title].any?
-  	assert product.errors[:description].any?
-  	assert product.errors[:price].any?
-  	assert product.errors[:image_url].any?
+describe Product do
+  it "has a valid factory" do
+    expect(create :product).to be_valid
   end
 
-  test "product price must be positive" do
-  	product = Product.new(title: 		"My Book Title",
-  						  description: 	"yyy",
-  						  image_url: 	"zzz.jpg")
-  	product.price = -1
-  	assert product.invalid?
-  	assert_equal ["must be greater than or equal to 0.01"],
-  		product.errors[:price]
-
-  	product.price = 0
-  	assert product.invalid?
-  	assert_equal ["must be greater than or equal to 0.01"],
-  		product.errors[:price]
-
-  	product.price = 1
-  	assert product.valid?
+  it "is invalid without title" do
+    expect(build :product, title: nil).not_to be_valid
   end
 
-  def new_product(image_url)
-  	Product.new(title: 			"My Book Title",
-  				description: 	"yyy",
-  				price: 			1,
-  				image_url: 		image_url)
+  it "has unique title" do
+    expect(create :product, title: "Steam Latin").to be_valid
+    expect(build :product, title: "Steam Latin").not_to be_valid
   end
 
-  test "image_url" do
-  	ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.jpg
-  			http://a.b.c/x/y/z/fred.gif}
-  	bad = %w{ fred.doc fred.gif/more fred.gif.more }
-  	ok.each do |name|
-  		assert new_product(name).valid?, "#{name} should be valid"
-  	end
-  	bad.each do |name|
-  		assert new_product(name).invalid?, "#{name} shouldn't be valid"
-  	end
+  it "has title longer than 3 signs" do
+    expect(build :product, title: "AB").not_to be_valid
+    expect(build :product, title: "ABC").to be_valid
   end
 
-  test "product is not valid without a unique title" do
-  	product = Product.new(title:   products(:ruby).title,
-  						  description: "yyy",
-  						  price:  1,
-  						  image_url: "fred.gif")
-
-  	assert product.invalid?
-  	assert_equal ["has already been taken"], product.errors[:title]
+  it "is invalid without description" do
+    expect(build :product, description: nil).not_to be_valid
   end
 
-  test "product is not valid without a unique title - i18n" do
-  	product = Product.new(title:   products(:ruby).title,
-  						  description: "yyy",
-  						  price:  1,
-  						  image_url: "fred.gif")
-
-  	assert product.invalid?
-  	assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+  it "is invalid without image url" do
+    expect(build :product, image_url: nil).not_to be_valid
   end
- end
+
+  it "has proper file extension in image url" do
+    expect(build :product, image_url: "lorem.jpg").to be_valid
+    expect(build :product, image_url: "lorem.gif").to be_valid
+    expect(build :product, image_url: "lorem.exe").not_to be_valid
+  end
+
+  it "is invalid without price" do
+    expect(build :product, price: nil).not_to be_valid
+  end
+
+  it "has price greater than 0" do
+    expect(build :product, price: 0.1).to be_valid
+    expect(build :product, price: 0).not_to be_valid
+    expect(build :product, price: -5).not_to be_valid
+  end
+
+  it "returns product updated latest" do
+    lorem = create :product
+    steam = create :product, title: "Steam engine"
+    expect(Product.latest.title).to eq(steam.title)
+    lorem.title = "ABC"
+    lorem.save
+    expect(Product.latest.title).to eq(lorem.title)
+  end
+
+  it "can not be destroyed if has any line items" do
+    a = create :line_item
+    p = create :product
+    p.line_items << a
+    p.save
+    expect(Product.count).to eq(1)
+    p.delete
+    expect(Product.count).to eq(1)
+  end
+end
